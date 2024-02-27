b1fc4d84faa174262935db17aa30f3772fa4a6a5
rlaehgns69 <rlaehgns69@naver.com>
2024-02-27 15:41:13 +0900

    resolve 다음 step 값을 넘길 때

--- js/promise2.js
+++ js/promise2.js
@@ -6,11 +6,11 @@ function main() {
     console.log(2)// 비동기라도 실행 setTimeout실행
     setTimeout(() => {
       console.log(4);
-      resolve();
+      resolve(1000000); //then의 매개변수 resolve 결과가 있을 때 then(다음 step)
     }, 3000); //12 3초후에 4출력해 3.then 비동기 then실행 4
   
-  }).then(()=>{
-    console.log("then 실행")
+  }).then((num)=>{
+    console.log(`${num}-then실행`);
   });
   console.log(3);
 }
