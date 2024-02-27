eb48af1d0e9d70985c4ba4e2b2cf7cc796138f5e
rlaehgns69 <rlaehgns69@naver.com>
2024-02-27 15:34:13 +0900

    Promise.then Promise만 쓰면 의미가 없다.

--- js/promise2.js
+++ js/promise2.js
@@ -1,10 +1,9 @@
 main();
 
 function main() {
-  setTimeout(() => {
-    console.log(1),
-    console.log(2);
-  }, 3000);//비동기 (함수정의, 시간) setTimeout. 사용 불가
-   //1먼저 실행하고 싶다. (3)-1-2 setTimeout안에 넣기
+  new Promise((resolve, reject) => {
+    console.log(1)
+    console.log(2)// 비동기라도 실행 setTimeout실행
+  }).then(()=>{});
   console.log(3);
 }
