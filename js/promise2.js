8e3eac6f29b59555ae60fa4e254981cb11f5259a
rlaehgns69 <rlaehgns69@naver.com>
2024-02-27 15:39:08 +0900

    Promise resolve 컨트롤 위치

--- js/promise2.js
+++ js/promise2.js
@@ -4,6 +4,13 @@ function main() {
   new Promise((resolve, reject) => {
     console.log(1)
     console.log(2)// 비동기라도 실행 setTimeout실행
-  }).then(()=>{});
+    setTimeout(() => {
+      console.log(4);
+      resolve();
+    }, 3000); //12 3초후에 4출력해 3.then 비동기 then실행 4
+  
+  }).then(()=>{
+    console.log("then 실행")
+  });
   console.log(3);
 }
